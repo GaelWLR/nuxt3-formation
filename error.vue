de29b42404aee1dcbd78401a08b388a6ec7866b0
Gaël Weiler <gweiler@kaliop.com>
2024-02-22 19:35:07 +0100

    Error handling done

---
+++ error.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps({
+  error: Object,
+});
+</script>
+
+<template>
+  <div>
+    <NuxtLayout>
+      <h1>Error {{ error?.statusCode }}</h1>
+      <h2>
+        {{ error?.message }}
+      </h2>
+    </NuxtLayout>
+  </div>
+</template>
--- pages/episodes/[id].vue
+++ pages/episodes/[id].vue
@@ -1,31 +1,54 @@
 <script setup lang="ts">
 import type { Character, Episode } from "~/types";
 
-const { $axios } = useNuxtApp();
+// throw new Error("Unexpected error in page setup.");
 
+const { $axios } = useNuxtApp();
 const route = useRoute();
 
 const episodeId = computed(() => route.params.id);
 
-const { data: episode } = await useAsyncData(
+const { data: episode, error } = await useAsyncData(
   `episode-${episodeId.value}`,
   async () => {
+    // throw new Error("Unexpected error during async data fetching.");
+
     const response = await $axios.get<Episode>(`/episode/${episodeId.value}`);
 
     return response?.data;
   }
 );
 
+if (error.value) {
+  throw createError({
+    fatal: true,
+    statusCode: 500,
+    message: "An error occurred while fetching the episode.",
+    cause: error.value?.cause,
+    stack: error.value?.stack,
+  });
+}
+
 const characters = ref<Character[]>([]);
 
-const fetchCharacters = async () => {
-  characters.value = await Promise.all(
-    episode.value?.characters.map(async (characterLink: string) => {
-      const response = await $axios.get<Character>(characterLink);
+const { openToast } = useToast();
 
-      return response?.data;
-    }) ?? []
-  );
+const fetchCharacters = async () => {
+  try {
+    // throw new Error("Unexpected error during characters fetching.");
+
+    characters.value = await Promise.all(
+      episode.value?.characters.map(async (characterLink: string) => {
+        const response = await $axios.get<Character>(characterLink);
+
+        return response?.data;
+      }) ?? []
+    );
+  } catch (e) {
+    console.error(e);
+
+    openToast("An error occurred while fetching characters.");
+  }
 };
 </script>
 
